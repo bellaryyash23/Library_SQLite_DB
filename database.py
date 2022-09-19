d203f433f9101e73fdb469de71b775bed94cf268
Yash Bellary <bellaryyash23@gmail.com>
2022-09-20 00:26:47 +0530

    Initial Commit

---
+++ database.py
@@ -0,0 +1,27 @@
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+#
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) "
+# #                "NOT NULL, rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+db = SQLAlchemy(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+all_books = db.session.query(Books).all()
